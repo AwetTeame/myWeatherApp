3267a306a92a55ca31231157022a7b37bd1460ea
Awet Teame <awettmt@gmail.com>
2023-11-03 09:53:18 +0300

    my first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '99ff35e568msh4092d41297954d7p1370e7jsn12b76bf5b4bc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "927e6b425e1f1eb7cd0a3847155c8362";
+
